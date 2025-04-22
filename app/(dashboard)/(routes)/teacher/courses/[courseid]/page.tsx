9b4ae83f5db3605cc5526dfd9bc8db373c8a58e4
kazinayeem <mohammadalinayeem360@gmail.com>
2025-04-22 18:36:58 +0600

    add title

--- app/(dashboard)/(routes)/teacher/courses/[courseid]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseid]/page.tsx
@@ -1,8 +1,74 @@
+import { IconBadge } from "@/components/icon-badge";
+import prisma from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import TitleForm from "./_components/title-form";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ courseid: string }>;
 }) {
   const { courseid } = await params;
-  return <div>My Post: {courseid}</div>;
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+  const course = await prisma.course.findUnique({
+    where: {
+      id: courseid,
+    },
+  });
+  if (!course) {
+    return redirect("/teacher/courses");
+  }
+
+  const requireFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.categoryId,
+    course.price,
+  ];
+
+  const totalFields = requireFields.length;
+  const completedFields = requireFields.filter(Boolean).length;
+  const completionText = `${completedFields} of ${totalFields} fields completed`;
+  const completionPercentage = Math.round(
+    (completedFields / totalFields) * 100
+  );
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-bold">Course SetUp</h1>
+          <p className="text-sm text-slate-600 dark:text-slate-400">
+            {completionText}
+          </p>
+          <div className="w-full bg-slate-200 dark:bg-slate-700 rounded-full h-2.5">
+            <div
+              className="bg-blue-600 h-2.5 rounded-full"
+              style={{ width: `${completionPercentage}%` }}
+            ></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge size={"sm"} icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+          <TitleForm
+            courseId={courseid}
+            initialData={{
+              title: course.title,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ app/api/courses/[courseid]/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseid: string } }
+) {
+  const { courseid } =await params;
+  try {
+    const { userId } = await auth();
+
+    const body = await req.json();
+
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    if (!courseid) {
+      return NextResponse.json(
+        { message: "Course ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const course = await prisma.course.update({
+      where: { id: courseid, userId: userId },
+      data: { ...body },
+    });
+
+    return NextResponse.json(
+      { message: "Course updated successfully", course: course },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("[Error updating course:]", error);
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
