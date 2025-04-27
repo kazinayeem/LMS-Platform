62658fbbac1985a8d4ae0ecbf9eaf2449ba9f40c
kazinayeem <mohammadalinayeem360@gmail.com>
2025-04-27 23:16:42 +0600

    fix

--- app/(dashboard)/(routes)/teacher/category/page.tsx
+++ app/(dashboard)/(routes)/teacher/category/page.tsx
@@ -3,11 +3,13 @@ import CategoryList from "./_components/category-list";
 import CategoryForm from "./_components/CategoryForm";
 import { LoadingSkeleton } from "@/components/loading";
 
-export default function Page() {
+export default async function Page() {
   return (
     <div className="flex flex-wrap gap-4 p-10">
       <div className="w-full sm:w-[calc(50%-0.5rem)] md:w-[calc(33.333%-0.5rem)] lg:w-[calc(25%-0.5rem)] flex flex-col justify-between">
-        <CategoryForm />
+        <Suspense fallback={<LoadingSkeleton />}>
+          <CategoryForm />
+        </Suspense>
       </div>
       <Suspense fallback={<LoadingSkeleton />}>
         <CategoryList />
