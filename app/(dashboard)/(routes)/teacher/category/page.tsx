a2149c795b2a7620f3801da0b41b4c24f1db19d0
kazinayeem <mohammadalinayeem360@gmail.com>
2025-04-28 09:07:04 +0600

    test 1

--- app/(dashboard)/(routes)/teacher/category/page.tsx
+++ app/(dashboard)/(routes)/teacher/category/page.tsx
@@ -1,16 +1,15 @@
+import { LoadingSkeleton } from "@/components/loading";
 import { Suspense } from "react";
 import CategoryList from "./_components/category-list";
-import CategoryForm from "./_components/CategoryForm";
-import { LoadingSkeleton } from "@/components/loading";
 
 export default async function Page() {
   return (
     <div className="flex flex-wrap gap-4 p-10">
-      <div className="w-full sm:w-[calc(50%-0.5rem)] md:w-[calc(33.333%-0.5rem)] lg:w-[calc(25%-0.5rem)] flex flex-col justify-between">
+      {/* <div className="w-full sm:w-[calc(50%-0.5rem)] md:w-[calc(33.333%-0.5rem)] lg:w-[calc(25%-0.5rem)] flex flex-col justify-between">
         <Suspense fallback={<LoadingSkeleton />}>
           <CategoryForm />
         </Suspense>
-      </div>
+      </div> */}
       <Suspense fallback={<LoadingSkeleton />}>
         <CategoryList />
       </Suspense>
